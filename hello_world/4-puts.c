043439a7598991fb1e8273acc75e917b5f48356e
Stefano-BML <stefanoml1128@gmail.com>
2023-02-20 11:09:43 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-/** 
+/**
  * main - A program that prints a line with puts funtion
- *
  * return: 0 (Success)
  */
 int main(void)
@@ -9,4 +8,3 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-	
