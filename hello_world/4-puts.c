6f23ab272a1c8860a7ac192c76fb8150c979c7e9
Stefano-BML <stefanoml1128@gmail.com>
2023-02-20 11:42:01 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry point
  *
- * Description: 'a program with puts funtion'
+ * Description: 'puts funtions'
  *
- * return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
