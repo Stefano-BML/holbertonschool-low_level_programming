0c7102cf77e262630ca1f419d5636791678e658e
Stefano-BML <stefanoml1128@gmail.com>
2023-02-21 19:52:42 -0800

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - double alphabet task
- * return: Always zero (Success)
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        int i;
+        char i;
 
         for(i = 'a'; i <= 'z'; i++)
         {
