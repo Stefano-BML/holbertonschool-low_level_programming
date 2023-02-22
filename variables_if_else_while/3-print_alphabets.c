9db29d577bc1f79b60f7f6631704e446351d83cb
Stefano-BML <stefanoml1128@gmail.com>
2023-02-21 19:58:15 -0800

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,18 +7,18 @@
  */
 
 int main(void)
-{	
+{
 	char i;
 
-        for(i = 'a'; i <= 'z'; i++)
-        {
-                putchar(i);
-        }
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
 	for(i = 'A'; i <= 'Z'; i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
+	{
+		putchar(i);
+	}
+	putchar('\n');
 
-        return(0);
+	return(0);
 }
