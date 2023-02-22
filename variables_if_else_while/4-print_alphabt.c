7d8fdc497586122f9a9746ab187d4b51271788b5
Stefano-BML <stefanoml1128@gmail.com>
2023-02-21 20:03:23 -0800

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,24 @@
-i
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
