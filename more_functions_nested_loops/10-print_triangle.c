bfe94cfebbd860718adc586c88f160ee295e9881
Stefano-BML <stefanoml1128@gmail.com>
2023-02-26 17:42:19 -0800

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -13,13 +13,12 @@ int i;
 int j;
 
 if (size <= 0)
-putchar('\n');
+_putchar('\n');
 for (i = 0; i < size; i++)
 {
 for (j = size - i - 1; j > 0; j--)
-putchar(' ');
+_putchar(' ');
 for (j = i + 1; j > 0; j--)
-putchar('#');
-putchar('\n');
-}
+_putchar('#');
+_putchar('\n');
 }
