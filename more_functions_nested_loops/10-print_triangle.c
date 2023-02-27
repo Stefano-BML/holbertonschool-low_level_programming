ec3370b8817b65da1dd53d85dc1e4128461aa8ab
Stefano-BML <stefanoml1128@gmail.com>
2023-02-26 17:40:27 -0800

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,27 +9,17 @@
 
 void print_triangle(int size)
 {
-	int i;
-	int j;
-	int k;
+int i;
+int j;
 
-	if (size > 0)
-	{
-		for (i = 1; i <= size; i++)
-		{
-			for (j = size; j > i; j--)
-			{
-				_putchar(' ');
-			}
-			for (k = 0; k > i; k++)
-			{
-				_putchar('#');
-			}
-		}
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (size <= 0)
+putchar('\n');
+for (i = 0; i < size; i++)
+{
+for (j = size - i - 1; j > 0; j--)
+putchar(' ');
+for (j = i + 1; j > 0; j--)
+putchar('#');
+putchar('\n');
+}
 }
