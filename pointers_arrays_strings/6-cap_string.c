c6ee6390d6c64bebf9670aba44fa097a7cc5afe8
Stefano-BML <stefanoml1128@gmail.com>
2023-03-03 23:06:17 -0800

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,31 +10,29 @@
 char *cap_string(char *str)
 {
 	char *tmp = str;
-	int i = 1;
+
+	int capitalize_next = 1;
 
 	while (*tmp != '\0')
 	{
+		if (capitalize_next && (*tmp >= 'a' && *tmp <= 'z'))
+		{
+			*tmp = *tmp - 32;
+		}
 		if (*tmp == ' ' || *tmp == '\t' || *tmp == '\n' ||
 			*tmp == ',' || *tmp == ';' || *tmp == '.' ||
 			*tmp == '|' || *tmp == '?' || *tmp == '"' ||
 			*tmp == '(' || *tmp == ')' || *tmp == '{' || *tmp == '}')
 		{
-			i = 1;
+			capitalize_next = 1;
 		}
 		else
 		{
-			if (i == 1)
-			{
-				if (*tmp >= 'a' && *tmp <= 'z')
-				{
-					*tmp = *tmp - 32;
-					i = 0;
-				}
-			}
+			capitalize_next = 0;
 		}
+
 		tmp++;
 	}
-
 	return (str);
 }
 
