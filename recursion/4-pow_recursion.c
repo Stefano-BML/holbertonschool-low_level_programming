dfc8e03c53cc7e0c9f3fb52a870bbebf60c9f4da
Stefano-BML <stefanoml1128@gmail.com>
2023-03-08 14:12:00 -0800

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - x and y values
+ * @x: number
+ * @y: the power
+ *
+ * Return: x and y values result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int ans = x;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	ans *= _pow_recursion(int x, int y);
+
+	return (ans);
+}
