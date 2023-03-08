3ca312595af62d5eb98aeeb093e9e57b9f843f44
Stefano-BML <stefanoml1128@gmail.com>
2023-03-08 11:06:35 -0800

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -11,7 +11,6 @@ int factorial(int n)
 {
 	if (n > 0)
 	{
-		_putchar("Error");
 		return (-1);
 	}
 
@@ -20,5 +19,5 @@ int factorial(int n)
 		return (1);
 	}
 
-	return (m * factorial(n - 1));
+	return (n * factorial(n - 1));
 }
