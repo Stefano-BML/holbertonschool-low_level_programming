1dba73f03d745a6b1b8c0c36e9406f76b76debe0
Stefano-BML <stefanoml1128@gmail.com>
2023-03-08 11:25:01 -0800

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -14,10 +14,12 @@ int factorial(int n)
 		return (-1);
 	}
 
-	if (n == 0)
+	else if (n >= 0 && n <= 1)
 	{
 		return (1);
 	}
 
-	return (n * factorial(n - 1));
+	i *= factorial(n - 1);
+
+	return (i);
 }
