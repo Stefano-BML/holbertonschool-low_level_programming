ab2cde62711f6082c3b5d2f7c5e1e2e2bcfa2905
Stefano-BML <stefanoml1128@gmail.com>
2023-03-11 19:26:34 -0800

    update

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,8 +1,8 @@
 int _putchar(char c);
 
-int main(int argc, charargv[]);
+int main(int argc, char *argv[]);
 
-int main(int argc, charargv[]__attribute((unused)));
+int main(int argc, char *argv[]__attribute((unused)));
 
 int main(int argc, char *argv[]);
 
