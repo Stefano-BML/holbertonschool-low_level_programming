c3ebac930dd8c46b276c6070e81c1adb1c09b068
Stefano-BML <stefanoml1128@gmail.com>
2023-03-18 07:58:03 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,6 +12,10 @@
 
 char *str_concat(char *s1, char *s2)
 {
+
+	int len1, len2;
+	char *result;
+
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -21,16 +25,17 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
+	
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
-
-	char *result = malloc(len1 + len2 + 1);
+	result = malloc(len1 + len2 + 1);
 
 	if (result == NULL)
 	{
 	return (NULL);
 	}
+
 	memcpy(result, s1, len1);
 	memcpy(result + len1, s2, len2 + 1);
 
