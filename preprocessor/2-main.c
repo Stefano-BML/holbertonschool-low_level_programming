3ee942b1c75736464d2495e00dcfcd51567a5ddf
Stefano-BML <stefanoml1128@gmail.com>
2023-03-19 18:59:26 -0700

    2-main.c

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - program that prints the name
+ *
+ * Return: Always zero
+ */
+
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
