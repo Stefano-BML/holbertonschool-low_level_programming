e1dfc56301239a279151fa89e09f390dd8fafda1
Stefano-BML <stefanoml1128@gmail.com>
2023-03-21 09:26:50 -0700

    update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -22,5 +22,10 @@ struct dog
 typedef struct dog dog_t;
 
 void init_dog(struct dog *i, char *name, float age, char *owner);
+void print_dog(struct dog *i);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
