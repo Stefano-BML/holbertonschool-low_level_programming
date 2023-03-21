73061114a1ad2b988c43edbd170818f8ba28d4d8
Stefano-BML <stefanoml1128@gmail.com>
2023-03-21 10:15:12 -0700

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,17 +17,7 @@ typedef struct dog
 	char *owner;
 }
 
-/**
- * dog_t - Typedef for struct dog
- */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *i, char *name, float age, char *owner);
-void print_dog(struct dog *i);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif
