e5da3d072059d66593ed69d999a545203bd27d1f
Stefano-BML <stefanoml1128@gmail.com>
2023-03-22 22:18:05 -0700

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* init_dog - my dog
+* @d: pointer
+* @name: name dog
+* @age: age dog
+* @owner: owner dog
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+}
