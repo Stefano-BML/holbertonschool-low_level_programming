33819f7d77d80e9762efcec4f5d9e96e3612863e
Stefano-BML <stefanoml1128@gmail.com>
2023-04-06 20:55:13 -0700

    fixe the betty style

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,16 +3,19 @@
 #include <string.h>
 
 /**
- * add_node - adds a new node at the end of a list_t list.
+ * add_node_end - adds a new node at the end of a list_t list.
  * @head: pointer to the pointer to the first element of the list
  * @str: string to be duplicated and added to the new node
  *
  * Return: Address of the new element, or NULL if it failed
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+
 	list_t *current_node;
+
 	current_node = *head;
 
 	if (head == NULL || str == NULL)
