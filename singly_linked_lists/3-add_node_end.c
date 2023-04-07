dd3bd41f1ed73ab1ded797fa8f8d7a8038de91ed
Stefano-BML <stefanoml1128@gmail.com>
2023-04-06 20:50:37 -0700

    add a declaration

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,6 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *current_node;
+	list_t current_node = *head;
 
 	if (head == NULL || str == NULL)
 		return (NULL);
